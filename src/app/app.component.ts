cd735fbdb94511191e4ead92aaf759bd2737ac6b
Ivan Melnyk <ivanmel.vn23@gmail.com>
2024-01-18 18:20:13 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PlotlyModule } from 'angular-plotly.js';
+import * as PlotlyJS from 'plotly.js-dist-min';
+import { FormsModule } from '@angular/forms';
+
+PlotlyModule.plotlyjs = PlotlyJS;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, PlotlyModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'plot-test';
+
+  number_y: any = 50;
+  number_x: any = 10;
+  graph = this.getNewValues();
+
+  refresh() {
+    this.graph = this.getNewValues();
+  }
+
+  getNewValues() {
+   return {
+      data: [
+          { z: getPointsArray(this.number_x, this.number_y), type: 'surface' },
+      ],
+      layout: {width: 600, height: 600, title: 'Test Surface Plot'},
+
+    }
+  }
+}
+
+function getRandomPoint(x: number): number[] {
+  let array = [];
+  for (let i = 0; i < x; i++) {
+    array.push(Math.random() * 200)
+  }
+    return array;
+}
+
+function getPointsArray(x: number, y: number) {
+  let array = [];
+  for(let i = 0; i < y; i++) {
+    array.push(getRandomPoint(x));
+  }
+  return array;
 }
